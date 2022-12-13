40c39c50097dd8327bdcdabe74f74992656c796d
Shibil-77 <shibi2772@gmail.com>
2022-12-13 15:54:46 +0530

    TODO completed

--- src/App.js
+++ src/App.js
@@ -14,27 +14,34 @@ function App() {
       </div>
       <div className="input">
         <input value={toDo} onChange={(e) => setTodo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
-        <i onClick={() => setTodos([...toDos,{id:Date.now(),text:toDo,status:false}])} className="fas fa-plus"></i>
+        <i onClick={() => setTodos([...toDos, { id: Date.now(), text: toDo, status: false }])} className="fas fa-plus"></i>
       </div>
       <div className="todos">
         {toDos.map((obj) => {
           return (
             <div className="todo">
               <div className="left">
-                <input onChange={(e)=>{
+                <input onChange={(e) => {
                   console.log(e.target.value)
                   console.log(obj)
-                  setTodos(toDos.filter(obj2=>{
-                    if(obj2.id===obj.id){
-                       obj2.status=e.target.checked
+                  setTodos(toDos.filter(obj2 => {
+                    if (obj2.id === obj.id) {
+                      obj2.status = e.target.checked
                     }
                     return obj2
                   }))
+
                 }} value={obj.status} type="checkbox" name="" id="" />
                 <p>{obj.text}</p>
               </div>
               <div className="right">
-                <i className="fas fa-times"></i>
+                <i className="fas fa-times" onClick={() => setTodos(toDos.filter(obj2 => {
+                  let temp;
+                  if (obj2.id !== obj.id) {
+                    temp = obj2
+                  }
+                  return temp;
+                }))} ></i>
               </div>
             </div>
           )
